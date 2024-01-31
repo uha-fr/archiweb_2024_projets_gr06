94a8722da94575481ee854c89033801eb0c8e54e
clstialdev <rusty75001@gmail.com>
2024-01-31 11:04:40 +0100

    add security and notifications setting page components

---
+++ src/views/components/user/profile/security.php
@@ -0,0 +1,56 @@
+<!-- SECURITY -->
+<div class="container-fluid pt-4">
+          <h4>Account Security</h4>
+          <p>Update your email and password here.</p>
+          <!-- FORM -->
+          <form class="" id="form-data">
+            <!-- EMAIL -->
+            <label for="email" class="pt-2">Email:</label>
+            <input
+              type="email"
+              class="form-control mt-2"
+              aria-label="email"
+              id="email"
+              name="email"
+              value="<?php echo $_SESSION['email'] ?>"
+            />
+            <!-- OLD PASSWORD -->
+            <label for="old_password" class="pt-2">Old Password</label>
+            <input
+              type="password"
+              class="form-control mt-2"
+              aria-label="old_password"
+              id="old_password"
+              name="old_password"
+            />
+
+            <!-- New Password -->
+            <label for="password" class="pt-4">New Password</label>
+            <div class="input-group mt-2 flex-nowrap">
+              <span class="input-group-text">Password</span>
+              <input
+                type="password"
+                aria-label="password"
+                class="form-control"
+                id="password"
+                name="new_password"
+              />
+              <span class="input-group-text">Repeat Password</span>
+              <input
+                type="password"
+                aria-label="repeat_password"
+                class="form-control"
+                id="repeat_password"
+                name="repeat_password"
+              />
+            </div>
+
+            <!-- BUTTON SUBMIT -->
+            <div class="d-grid gap-2 col-6 mt-4">
+              <button class="btn btn-primary bg-main border-main" id="update-user-credentials-btn" type="submit">
+                Submit
+              </button>
+            </div>
+          </form>
+        </div>
+
