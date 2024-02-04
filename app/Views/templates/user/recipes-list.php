d7f12242fa4c88cd26fe48906d456849c94c4856
khedirwassim <88117655+khedirwassim@users.noreply.github.com>
2024-02-05 00:45:00 +0100

    Update recipes-list.php

--- app/Views/templates/user/recipes-list.php
+++ app/Views/templates/user/recipes-list.php
@@ -4,7 +4,7 @@
 <head>
     <meta charset="UTF-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <title>Login</title>
+    <title>Recipes List</title>
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" />
     <link rel="stylesheet" href="<?= BASE_APP_DIR ?> /public/css/colors.css" />
     <script src="https://cdn.tailwindcss.com"></script>
@@ -14,16 +14,16 @@
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/css/bootstrap.min.css">
     <link href="https://cdn.datatables.net/v/dt/dt-1.13.8/datatables.min.css" rel="stylesheet">
     <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.15.4/css/all.css">
-
 </head>
 
 <body>
     <!-- HEADER -->
+     include_once VIEWSDIR . DS . 'components' . DS . 'header.php';
     <?php
-    include_once 'header.php';
+    
     ?>
     <div class="w-full min-h-screen pl-[180px] bg-bg">
-        <!-- Login Form-->
+        <!-- Recipes Form-->
         <div class="w-full items-center flex-column flex min-h-screen pt-24">
             <h1 class="text-5xl font-bold">Recipes</h1>
             <div class="container">
@@ -52,20 +52,10 @@
     </div>
     </div>
     </div>
-    <!-- Correct Order and Single Version of jQuery -->
-    <script src="https://code.jquery.com/jquery-3.7.1.min.js"></script>
-    <!-- DataTables CSS -->
-    <link href="https://cdn.datatables.net/1.13.8/css/jquery.dataTables.min.css" rel="stylesheet">
-    <!-- DataTables JS -->
-    <script src="https://cdn.datatables.net/1.13.8/js/jquery.dataTables.min.js"></script>
-    <!-- Bootstrap and Other Dependencies -->
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/css/bootstrap.min.css">
-    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js"></script>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/js/bootstrap.bundle.min.js"></script>
+   
     <script src="<?= BASE_APP_DIR ?>/public/js/ajax.js"></script>
     <script type="text/javascript">
         $(document).ready(function() {
-
             performAjaxRequest(
                 "POST",
                 "showAllRecipes",
@@ -76,5 +66,4 @@
         });
     </script>
 </body>
-
 </html>
