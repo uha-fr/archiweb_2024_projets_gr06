b9d386ea671904e9493b5c938ceb4e1cbb1c61e3
clstialdev <rusty75001@gmail.com>
2024-02-05 11:19:44 +0100

    removed tailwind from login

--- app/Views/templates/user/login.php
+++ app/Views/templates/user/login.php
@@ -7,7 +7,6 @@
   <title>Login</title>
   <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" />
   <link rel="stylesheet" href="<?= BASE_APP_DIR ?>/public/css/colors.css" />
-  <script src="https://cdn.tailwindcss.com"></script>
   <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
   <script src="https://code.jquery.com/jquery-3.7.1.min.js"></script>
   <link rel="stylesheet" href="<?= BASE_APP_DIR ?>/public/css/globals.css" />
@@ -23,31 +22,30 @@
 
   ?>
   <!-- BODY -->
-  <div class="w-full min-h-screen pl-[180px] bg-bg">
+  <div class="container-fluid bg-bg" style="padding-left: 180px; min-height: 100vh;">
     <!-- Login Form-->
-    <div class="w-full items-center flex-column flex min-h-screen pt-24">
-      <h1 class="text-5xl font-bold">Login</h1>
-      <form class="bg-gray w-[500px] rounded min-h-[400px] mt-14 p-8" id="form-data" action="" method="post">
+    <div class="container-fluid align-items-center flex-column d-flex" style="min-height: 100vh; padding-top: 140px;">
+      <h1 class="fw-bold">Login</h1>
+      <form class="bg-gray rounded" style="width: 500px; min-height: 400px; margin-top: 40px; padding: 32px;" id="form-data" action="" method="post">
 
-        <div class="flex flex-column mt-4">
+        <div class="d-flex flex-column mt-4">
           <label class="font-bold text-white">Email</label>
-          <input type="email" name="email" placeholder="Ex:john.doe@gmail.com" class="py-3 px-4 rounded mt-2" required />
+          <input type="email" name="email" placeholder="Ex:john.doe@gmail.com" class="py-3 px-4 rounded mt-2" style="border: 0;" required />
         </div>
-        <div class="flex flex-column mt-4">
+        <div class="d-flex flex-column mt-4">
           <label class="font-bold text-white">Password</label>
-          <input type="password" id="password" name="password" placeholder="Password" class="py-3 px-4 rounded mt-2" required />
+          <input type="password" id="password" name="password" placeholder="Password" class="py-3 px-4 rounded mt-2" style="border: 0;" required />
         </div>
 
 
 
-        <div class="flex flex-column mt-4">
-          <input type="submit" class="py-3 px-4 bg-[#d6ff92] rounded w-full" name="login" id="login" value="Login">
+        <div class="d-flex flex-column mt-4">
+          <input type="submit" class="py-3 px-4 rounded w-full" style="background-color: #d6ff92; border: 0; margin-bottom: 12px;" name="login" id="login" value="Login">
         </div>
-        <a class="logo h-[66px] w-[66px] self-end" href="reset-password">
-          Reset password
-          <img src="" alt="" />
+        <a class="logo self-end" href="reset-password">
+          Reset password </br>
         </a>
-        <a class="logo h-[66px] w-[66px] self-end" href="register">
+        <a class="logo self-end" href="register">
           Dont have an account?
         </a>
       </form>
