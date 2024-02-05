187aae724466c45bf2d167172d7ee52c3e3ccd67
Elias Mouaheb <elias.mouaheb@etu.unistra.fr>
2024-02-05 19:56:07 +0100

    doc done on several files

--- app/Views/utils/global.php
+++ app/Views/utils/global.php
@@ -1,9 +1,24 @@
 <?php
-function start_stream(){
-		ob_start();
+
+/**
+ * Initiates output buffering using the ob_start() function.
+ *
+ * @return void
+ */
+function start_stream()
+{
+	ob_start();
 };
 
-function end_stream(){
+/**
+ * Ends output buffering.
+ * 
+ * Retrieves the buffered content using ob_get_clean(), and returns the captured content.
+ *
+ * @return string The captured content from the output buffer.
+ */
+function end_stream()
+{
 	$content = ob_get_clean();
-    return $content;
+	return $content;
 }
