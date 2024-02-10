22d9421a1d3b457bbd1467ef5b4826b5d4ea1917
Elias Mouaheb <elias.mouaheb@etu.unistra.fr>
2024-02-10 22:21:25 +0100

    added db script, fixed css paths

--- app/Views/templates/user/recipes-list.php
+++ app/Views/templates/user/recipes-list.php
@@ -8,11 +8,11 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.8.0/font/bootstrap-icons.css" rel="stylesheet">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.8.0/font/bootstrap-icons.css" rel="stylesheet">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" />
-    <link rel="stylesheet" href="<?= BASE_APP_DIR ?> /public/css/colors.css" />
+    <link rel="stylesheet" href="<?= BASE_APP_DIR ?>/public/css/colors.css" />
     <script src="https://cdn.tailwindcss.com"></script>
     <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
     <script src="https://code.jquery.com/jquery-3.7.1.min.js"></script>
-    <link rel="stylesheet" href="<?= BASE_APP_DIR ?> /public/css/global.css" />
+    <link rel="stylesheet" href="<?= BASE_APP_DIR ?>/public/css/global.css" />
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/css/bootstrap.min.css">
     <link href="https://cdn.datatables.net/v/dt/dt-1.13.8/datatables.min.css" rel="stylesheet">
     <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.15.4/css/all.css">
