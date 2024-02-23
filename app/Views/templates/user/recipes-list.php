9cb92a31dd22b6a71c755d44c59361d6c6b21f92
khedirwassim <88117655+khedirwassim@users.noreply.github.com>
2024-02-23 08:47:56 +0100

    Update recipes-list.php

--- app/Views/templates/user/recipes-list.php
+++ app/Views/templates/user/recipes-list.php
@@ -1,6 +1,5 @@
 <!DOCTYPE html>
 <html lang="en">
-
 <head>
     <meta charset="UTF-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
@@ -18,16 +17,12 @@
     <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.15.4/css/all.css">
     <script src="<?= BASE_APP_DIR ?>/public/js/recipes.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"></script>
-
-
 </head>
-
 <body>
     <!-- HEADER -->
     <?php
     include_once VIEWSDIR . DS . 'components' . DS . 'header.php';
     ?>
-
     <div class="w-full min-h-screen pl-[180px] bg-bg">
         <!-- Recipes Form-->
         <div class="container">
@@ -40,21 +35,17 @@
                 <div class="col-lg-6">
                 </div>
                 <div class="d-flex align-items-center justify-content-end">
-                    <button class="btn btn-lg btn-main-color text-white"
+                    <button class="btn btn-success btn-lg float-right"
                         style="background-color: #678e48; border: 0; margin-bottom: 12px;" data-toggle="modal"
-                        data-target="#addRecipeModal">
+                        data-target="#addModel" onclick="toggleIframe()">
                         <span class="me-2" style="font-size: 1.2em;">Add New Recipe</span>
                         <i class="fas fa-plus-circle fa-lg" style="font-size: 1.2em;"></i>
-                    </button>
-                    
-                </div>
-              
+                    </button>              
+                </div>             
             </div>
             <div class="row justify-content-center">
-
-            
-
-
+                <!-- iframe à cacher/afficher -->
+                <iframe id="myIframe" src="add-recipe" height="700" class="hidden"></iframe>
                 <div class="row">
                     <div class="container-fluid bg-main rounded-3 pb-4 d-flex justify-content-center align-items-center p-4"" style="
                         min-height: 300px;">
