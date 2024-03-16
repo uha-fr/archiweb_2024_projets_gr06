b2d26daec8b7b05c8ab32232a7dc7505de02af3e
eisenhowair <103502603+eisenhowair@users.noreply.github.com>
2024-03-16 23:11:20 +0100

    Update add-recipe.php

--- app/Views/templates/user/add-recipe.php
+++ app/Views/templates/user/add-recipe.php
@@ -6,6 +6,8 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" />
     <link rel="stylesheet" href="<?= BASE_APP_DIR ?>/public/css/colors.css" />
+    <link rel="stylesheet" href="<?= BASE_APP_DIR ?>/public/css/globals.css" />
+
     <script src="https://cdn.tailwindcss.com"></script>
     <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
     <script src="https://code.jquery.com/jquery-3.7.1.min.js"></script>
