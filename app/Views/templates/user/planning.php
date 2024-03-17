8685f7e76ac0982dd4751e8f603201bd30bd113c
eisenhowair <103502603+eisenhowair@users.noreply.github.com>
2024-03-17 11:22:14 +0100

    Update planning.php
    Changed title from login to planning

--- app/Views/templates/user/planning.php
+++ app/Views/templates/user/planning.php
@@ -19,7 +19,7 @@ $durationJson = json_encode($duration);
 <head>
     <meta charset="UTF-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <title>Login</title>
+    <title>Planning</title>
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" />
     <link rel="stylesheet" href="<?= BASE_APP_DIR ?>/public/css/colors.css" />
     <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
